0be548f43974ba1ad44e22c01a888c7d22a49c66
Miochelt1@ <thiago.m.a@outlook.com>
2024-01-23 19:21:06 -0300

    efetuado conficuracoes inicial back

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
+//ROTA
+//METHOD HTTP COMO NOS COMUNICAMOS COM CLIENTE E SERVIDOR  - CRUD(CREATE READ UPDATE DELETE )
+//METHOD POST - CRIA UMA INFO
+//METHOD GET -BUSCA UMA INFO 
+//METHOD PUT - ALTERA UMA INFO
+//METHOD DELETE APAGA UMA INFO
+//METHOD PATCH ALTERA PARTE DA INFO
 
-app.get('/',  (req, res)=> {
-  res.send('Hello World')
-})
+//NAME
+//funcao anonima (callback) responsavel por executar algum comando porque callback chamada por traz de uma outra funcao  
+app.get("/ route", (req, res)=>{});
+
 
 app.listen(3000)
